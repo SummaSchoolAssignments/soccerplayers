ca7a5478164155c5aa26c0312a42cc71cd9234c7
DaintyDust <144209886+DaintyDust@users.noreply.github.com>
2025-09-19 11:41:35 +0200

    opdracht

---
+++ database/migrations/2025_09_19_093948_create_athletes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('athletes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('sport', 25);
+            $table->string('club', 80);
+            $table->integer('salary')->nullable();
+            $table->boolean('active');
+            $table->date('date_of_birth');
+            $table->integer('total_goals');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('athletes');
+    }
+};
